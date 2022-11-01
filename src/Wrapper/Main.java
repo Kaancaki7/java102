450ad76c8829b3adcdb274124e2730b99cde6f21
kaan <kaancaki0@gmail.com>
2022-11-01 17:55:51 +0300

    java102

---
+++ src/Wrapper/Main.java
@@ -0,0 +1,19 @@
+package Wrapper;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Integer a = 10;
+        String b = "str";
+        Short s = 1;
+
+        String str = "10";
+        int number = 20;
+        System.out.println(Integer.parseInt(str) + number);
+
+        //Byte-Integer-Short-Long-Float-Double-Boolean-Character
+        //Bunlar nesne olarak kullanmamız i.in bize bir yol gösteeriyorlar
+        //tıpkı Integer.parseInt metodu gibi string'i int e çeviriyor
+        //Burada hepsini yazamayız bir çok metodu var hepsinin lazım olduğunda bak.
+    }
+}
