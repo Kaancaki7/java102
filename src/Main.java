4d6a88ad46099dcfe0ad1be81f0192e22ae560db
kaan <kaancaki0@gmail.com>
2022-12-13 19:08:21 +0300

    gelecegiyazanlar

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int i = 5;
+
+        String s = i == 5 ? "i degeri 5" : "i 5 degildir.";
+        System.out.println(s);
     }
 }
--- src/gelecegiyazanlar/sonpart/streamapi3/Driver.java
+++ src/gelecegiyazanlar/sonpart/streamapi3/Driver.java
@@ -18,7 +18,7 @@ public class Driver {
                 .collect(Collectors.toList());
 
         collect.forEach(System.out::println);
-        
+
 
 
     }
